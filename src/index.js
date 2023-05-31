37e8be7037d9efc2bb2e730da22963ca446096af
jqwirq <dummy.wiji@gmail.com>
2023-05-31 16:11:16 +0700

    update

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ const {
   updateProduct,
   deleteProduct,
 } = require("./controllers/productController");
+const dgram = require("dgram");
 
 const port = process.env.PORT || 3001;
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/mytest";
+
+let udpMessage = "0";
+let sseId = null;
 
 router.post("/products-csv", importProductsFromCSVs);
 router.post("/product", createProduct);
@@ -24,6 +28,29 @@ router.get("/product/:no", getProduct);
 router.put("/product/:id", updateProduct);
 router.delete("/product/:id", deleteProduct);
 // router.delete('/product/:productId/material/:materialId', deleteMaterialFromProduct);
+
+router.get("/events", (req, res) => {
+  // Set response headers
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  const msg = `${udpMessage}\n\n`;
+  res.write(msg);
+
+  // Send an event every second
+  sseId = setInterval(() => {
+    const message = `data: ${udpMessage}\n\n`;
+    if (!res.write(message)) {
+      clearInterval(sseId);
+    }
+  }, 1000);
+
+  // Close event if client disconnects
+  req.on("close", () => {
+    clearInterval(sseId);
+  });
+});
 
 main();
 
@@ -34,11 +61,20 @@ async function main() {
     console.error(err.message); // This is bad
   }
 
+  const udpServer = dgram.createSocket("udp4");
+
   server.use(express.json());
   server.use(cors());
 
   server.use("/api", router);
 
+  udpServer.on("message", (msg, rinfo) => {
+    console.log(
+      `UDP server received ${msg} from ${rinfo.address}:${rinfo.port}`
+    );
+    udpMessage = msg.toString(); // Save latest message
+  });
+  udpServer.bind(3003);
   server.listen(port, listenCallback);
 }
 

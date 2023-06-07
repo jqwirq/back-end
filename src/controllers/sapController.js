822c216bd4aa848507cfb3230652c3353733df37
jqwirq <dummy.wiji@gmail.com>
2023-06-07 14:18:29 +0700

    update

---
+++ src/controllers/sapController.js
@@ -0,0 +1,44 @@
+const SAP = require("../models/sap");
+
+async function getAllSAP(req, res) {
+  try {
+    let { limit, offset, no, startDate, endDate } = req.query;
+
+    // Parse limit and offset to integers
+    limit = limit ? parseInt(limit) : undefined;
+    offset = offset ? parseInt(offset) : undefined;
+
+    // Prepare the query object
+    const query = {
+      isCompleted: true,
+    };
+    if (no) {
+      query.no = no;
+    }
+    if (startDate || endDate) {
+      query.createdAt = {};
+      if (startDate) {
+        // Start of the day
+        query.createdAt.$gte = new Date(
+          new Date(startDate).setHours(0, 0, 0, 0)
+        );
+      }
+      if (endDate) {
+        // End of the day
+        query.createdAt.$lte = new Date(
+          new Date(endDate).setHours(23, 59, 59, 999)
+        );
+      }
+    }
+
+    const completedSAPs = await SAP.find(query).skip(offset).limit(limit);
+
+    res.status(200).json(completedSAPs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports = {
+  getAllSAP,
+};
